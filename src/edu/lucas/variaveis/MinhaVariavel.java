0e30a8a90391c3d87c437562f96608b3f42cbc47
Bassetin <bassetolucas.dev@gmail.com>
2024-05-02 16:26:30 -0300

    Calculadora feita para testes educacionais

--- src/edu/lucas/variaveis/MinhaVariavel.java
+++ src/edu/lucas/variaveis/MinhaVariavel.java
@@ -8,46 +8,54 @@ public class MinhaVariavel {
     public static void main (String[] args){ 
     Scanner s = new Scanner(System.in);
 
+    System.out.println("Digite o seu nome:");
+    String nome = "";
+    nome = s.nextLine();
+
     int num1 = 0;
     int num2 = 0;
-    String opcoes = "Somar", "Sub","Div","Mult";
+    System.out.println("Digite um número:");
+    num1 = s.nextInt();
+
+    System.out.println("Digite outro número");
+    num2 = s.nextInt();
+
+    int opcoes;
+
     int resultado;
 
     System.out.println("Digite uma opção:");
-    String opcoes = s.nextLine();
+    System.out.println("1 - somar,2 - multiplicar, 3 - divisão e 4 - subtração.");
+
+    opcoes = s.nextInt();
+
 
 
     switch (opcoes) {
 
-        case "Somar" : 
+        case 1:
             resultado = num1 + num2;
-             System.out.println(resultado);
+             System.out.println(nome + "O resultado é :" + resultado);
             break;
 
-        case "Sub" :
+        case 2 :
             resultado = num1 - num2;
-             System.out.println(resultado);
+            System.out.println(nome + "O resultado é :" + resultado);
             break;
 
-        case "Div" :
+        case 3:
             resultado = num1 / num2;
-             System.out.println(resultado);
+            System.out.println(nome + "O resultado é :" + resultado);
             break;
 
-        case "Mult" :
+        case 4 :
             resultado = num1 * num2;
-              System.out.println(resultado);
+            System.out.println(nome + "O resultado é :" + resultado);
             break;
 
-        default :    
+        default:
             System.out.println("Essa operação não existe");
             break;
-            
-            
-
-        
-
-        
     }
 
 
