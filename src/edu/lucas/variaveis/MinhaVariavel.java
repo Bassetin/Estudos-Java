37e11920cc9cd9c0bbd5cc9df324343362793968
Bassetin <bassetolucas.dev@gmail.com>
2024-05-02 13:22:06 -0300

    Commit com alguns tipos de variáveis.

---
+++ src/edu/lucas/variaveis/MinhaVariavel.java
@@ -0,0 +1,29 @@
+package edu.lucas.variaveis;
+
+public class MinhaVariavel {
+
+    int minhaIdade = 21;
+    String nome = "Lucas";
+    double peso = 80.0;
+    boolean casado = false;
+    char homemMulher = 'H';
+
+    public static void main (String[] args){
+
+          MinhaVariavel var = new MinhaVariavel();
+
+          System.out.println(var.nome);
+          System.out.println(var.minhaIdade);
+          System.out.println(var.peso);
+          System.out.println(var.casado);
+          System.out.println(var.homemMulher);
+
+
+
+
+
+
+    }
+
+
+}
